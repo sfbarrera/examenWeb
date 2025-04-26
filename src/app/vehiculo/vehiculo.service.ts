162fbdd8aabbfeb033c2c81097af6b1883862269
sfbarrera <sf.barreram1@uniandes.edu.co>
2025-04-25 22:16:47 -0500

    Cree el servicio para pedir el API

---
+++ src/app/vehiculo/vehiculo.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Vehiculo } from './vehiculo.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VehiculoService {
+  /* URL del API que aporta el JSON con los vehiculos */
+  private apiUrl = 'https://gist.githubusercontent.com/josejbocanegra/17bb8c76405e43655d551a90800c8a81/raw/d41b4acc3457e51e7533fad6d5e9925ee9676457/202212_MISW4104_Grupo1.json';
+
+  constructor(private http: HttpClient) { }
+
+  getVehiculos(): Observable<Vehiculo[]> {
+    return this.http.get<Vehiculo[]>(this.apiUrl);
+  }
+  
+}
