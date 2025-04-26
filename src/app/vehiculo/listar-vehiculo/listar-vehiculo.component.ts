2e0c042d868283a05135e42dbb5d4c7501a5e540
sfbarrera <sf.barreram1@uniandes.edu.co>
2025-04-25 23:09:12 -0500

    Se actualiza la pagina HTML y CSS basico sencillo, y funcion de sumar las marcas

--- src/app/vehiculo/listar-vehiculo/listar-vehiculo.component.ts
+++ src/app/vehiculo/listar-vehiculo/listar-vehiculo.component.ts
@@ -13,12 +13,28 @@ import { VehiculoService } from '../vehiculo.service';
 export class ListarVehiculoComponent implements OnInit{
   vehiculos: Vehiculo[] = [];
 
+  marcasTotales: { nombre: string; total: number }[] = [];
+
   constructor(private vehiculoService: VehiculoService) {}
 
   ngOnInit(): void {
     this.vehiculoService.getVehiculos().subscribe(data => {
       this.vehiculos = data;
+
+      /* Calculo de totales */
+      this.calcularTotales();
     });
+  }
+
+  calcularTotales(): void {
+    const contador: { [marca: string]: number } = {};
+    for (let v of this.vehiculos) {
+      contador[v.marca] = (contador[v.marca] || 0) + 1;
+    }
+    this.marcasTotales = Object.keys(contador).map(marca => ({
+      nombre: marca,
+      total: contador[marca]
+    }));
   }
 
 }
